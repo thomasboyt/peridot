d3574b78a8f1076fd22869d1766a73013e6041fc
Thomas Boyt <thomas@ledgerx.com>
2015-08-12 14:02:58 -0400

    Add grunt deploy

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    secret: grunt.file.readYAML('_private.yml'),
+
+    // TODO: This won't rm removed resources automatically!
+    sftp: {
+      options: {
+        path: '<%= secret.ssh_path %>',
+        host: '<%= secret.ssh_host %>',
+        username: '<%= secret.ssh_username %>',
+        agent: process.env.SSH_AUTH_SOCK,
+        showProgress: true,
+        srcBasePath: '_site/',
+        createDirectories: true
+      },
+
+      all: {
+        files: {
+          './': ['_site/**']
+        }
+      }
+    },
+
+    exec: {
+      build: 'node index.js build'
+    }
+
+  });
+
+  grunt.registerTask('deploy', ['exec:build', 'sftp']);
+};
