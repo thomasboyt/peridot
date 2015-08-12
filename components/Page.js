05edfdcd8daa89f161267b41bd1ced2b2a786bf8
Thomas Boyt <thomas@ledgerx.com>
2015-08-12 15:04:08 -0400

    I found a font

--- components/Page.js
+++ components/Page.js
@@ -11,6 +11,7 @@ const Page = React.createClass({
       <html>
         <head>
           <title>{this.props.title}</title>
+          <link href="http://fonts.googleapis.com/css?family=Dosis:400,500,600,700" rel="stylesheet" type="text/css" />
           <link href="/normalize.css" rel="stylesheet" />
           <link href="/style.css" rel="stylesheet" />
         </head>
