f9625b4337bf5b0e9f331feaa22a79a61ce4a93e
Thomas Boyt <me@thomasboyt.com>
2015-08-12 19:17:07 -0400

    Add video playback

--- components/Video.js
+++ components/Video.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import _ from 'lodash';
 
 const Video = React.createClass({
+  renderSources(variants) {
+    const bestMp4 =
+      _.chain(variants)
+        .filter({content_type: 'video\/mp4'})
+        .max((variant) => variant.bitrate)
+        .value();
+
+    const bestWebm =
+      _.chain(variants)
+        .filter({content_type: 'video\/webm'})
+        .max((variant) => variant.bitrate)
+        .value();
+
+    return [
+      <source src={bestMp4.url} type="video/mp4" key="video/mp4" />,
+      <source src={bestWebm.url} type="video/web" key="video/web" />
+    ];
+  },
+
   render() {
     const media = this.props.media;
 
     return (
-      <img src={`${media.media_url_https}:small`} key={media.id_str} />
+      <video controls>
+        {this.renderSources(media.video_info.variants)}
+      </video>
     );
   }
 });
