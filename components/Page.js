ea6bd3ab5aa2e50808ee694eabfde7e99fbc0e8a
Thomas Boyt <me@thomasboyt.com>
2015-08-12 20:09:17 -0400

    Add Open Sans: 'At least it's not Helvetica'(tm)

--- components/Page.js
+++ components/Page.js
@@ -12,6 +12,7 @@ const Page = React.createClass({
         <head>
           <title>{this.props.title}</title>
           <link href="http://fonts.googleapis.com/css?family=Dosis:400,500,600,700" rel="stylesheet" type="text/css" />
+          <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css' />
           <link href="/normalize.css" rel="stylesheet" />
           <link href="/style.css" rel="stylesheet" />
         </head>
