037f78efba1690f77a4ed21e16a54538c91203e7
Thomas Boyt <me@thomasboyt.com>
2015-08-16 19:13:52 -0400

    Add link to gist explaining node react issue

--- src/injectGlobals.js
+++ src/injectGlobals.js
@@ -2,5 +2,6 @@
 global.__PROJECT__ = process.cwd();
 
 // Global React is used so components imported from project directory use the same instance of
-// React
+// React *in Node*
+// See: https://gist.github.com/thomasboyt/873ee81a5ddf65a1af08
 global.React = require('react');
