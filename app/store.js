a02219483069688f671f90d605517be341687621
Thomas Boyt <thomas@ledgerx.com>
2015-08-17 16:37:02 -0400

    Remove window.data TODO

--- app/store.js
+++ app/store.js
@@ -5,7 +5,6 @@ const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 import entries from './reducers/entries';
 
-// TODO: insert window.__data__ here
 export default function createAppStore(data) {
   return createStoreWithMiddleware(entries, data);
 }
