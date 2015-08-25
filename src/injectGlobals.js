c413d117620eed314b5c01f7c77ede2e207d4cad
Thomas Boyt <thomas@ledgerx.com>
2015-08-25 17:52:29 -0400

    Stub window in server rendering

--- src/injectGlobals.js
+++ src/injectGlobals.js
@@ -8,3 +8,7 @@ global.React = require('react');
 
 // Same dealio. Stateful components :(
 global.DocumentTitle = require('react-document-title');
+
+// TODO: I don't love the idea of stubbing out web APIs like this...
+// There might be an alternative through JSDOM? ask around
+global.window = {};
