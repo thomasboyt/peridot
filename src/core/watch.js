1dcd827db9eadc925d74ed3165ec926d6d10360b
Thomas Boyt <thomas@ledgerx.com>
2015-09-09 22:57:36 -0400

    [CLI] Only rebuild webpack when styles change

--- src/core/watch.js
+++ src/core/watch.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import chokidar from 'chokidar';
+import pathIsInside from 'path-is-inside';
 
 import errorWrap from '../util/errorWrap';
 import build from './build';
@@ -24,9 +25,20 @@ async function rebuild() {
 
   console.log(`File "${changedPath}" changed...`);
 
-  // TODO: only run subset based on changes
-  if (path.relative(process.cwd(), changedPath) === '_entries.yml') {
+  // Non-absolute paths are relative to cwd
+  let absPath;
+  if (!path.isAbsolute(changedPath)) {
+    absPath = path.join(process.cwd(), changedPath);
+  } else {
+    absPath = changedPath;
+  }
+
+  const stylesDir = path.join(process.cwd(), 'styles');
+
+  if (changedPath === '_entries.yml') {
     await build(['entries', 'pages']);
+  } else if (pathIsInside(absPath, stylesDir)) {
+    await build(['webpack']);
   } else {
     await build(['pages', 'webpack']);
   }
@@ -43,7 +55,9 @@ export default function watch() {
     '_entries.yml',
     'app/',
     'styles/',
-    path.join(__dirname, '../app')
+
+    // watch Peridot app folder
+    path.join(__dirname, '../../app')
   ], {
     // ignore dotfiles
     ignored: /[\/\\]\./,
